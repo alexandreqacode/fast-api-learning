b03dc7d9086936eb54356e70b7869a01f9c7f79d
Alexandre <alexandre.oliv.junior@gmail.com>
2024-08-11 22:20:49 -0300

    first commit

---
+++ books.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+BOOKS = [
+    {'title': 'Title One', 'author': 'Author One', 'category': 'science'},
+    {'title': 'Title Two', 'author': 'Author Two', 'category': 'science'},
+    {'title': 'Title Three', 'author': 'Author Three', 'category': 'history'},
+    {'title': 'Title Four', 'author': 'Author Four', 'category': 'math'},
+    {'title': 'Title Five', 'author': 'Author Five', 'category': 'math'},
+    {'title': 'Title Six', 'author': 'Author Six', 'category': 'math'},
+]
+
+print(type(BOOKS))
+
+@app.get("/books") # @app.get determina que será um endpoint e chamará a função "/nome do endpoint"
+async def read_all_books():
+    return BOOKS
