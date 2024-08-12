a6bcd205f3706c2ea4ec5dc57e0f6602464deec1
Alexandre <alexandre.oliv.junior@gmail.com>
2024-08-11 23:57:44 -0300

    second commit

--- books.py
+++ books.py
@@ -17,3 +17,11 @@ print(type(BOOKS))
 @app.get("/books") # @app.get determina que será um endpoint e chamará a função "/nome do endpoint"
 async def read_all_books():
     return BOOKS
+
+
+@app.get("/books/{book_title}")
+async def read_book(book_title: str):
+    for book in BOOKS:
+        if book.get('title').casefold() == book_title.casefold():
+            return book
+
