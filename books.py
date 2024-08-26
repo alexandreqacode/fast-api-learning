c64b2397064b608cd7bc2a1945a5e9329ecdeeb3
Alexandre <alexandre.oliv.junior@gmail.com>
2024-08-25 22:40:53 -0300

    query parameters

--- books.py
+++ books.py
@@ -2,7 +2,6 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-
 BOOKS = [
     {'title': 'Title One', 'author': 'Author One', 'category': 'science'},
     {'title': 'Title Two', 'author': 'Author Two', 'category': 'science'},
@@ -14,7 +13,8 @@ BOOKS = [
 
 print(type(BOOKS))
 
-@app.get("/books") # @app.get determina que será um endpoint e chamará a função "/nome do endpoint"
+
+@app.get("/books")  # @app.get determina que será um endpoint e chamará a função "/nome do endpoint"
 async def read_all_books():
     return BOOKS
 
@@ -25,3 +25,12 @@ async def read_book(book_title: str):
         if book.get('title').casefold() == book_title.casefold():
             return book
 
+
+# query parameters
+@app.get("/books/")
+async def read_category_by_query(category: str):
+    books_to_return = []
+    for book in BOOKS:
+        if book.get('category').casefold() == category.casefold():
+            books_to_return.append(book)
+    return books_to_return
